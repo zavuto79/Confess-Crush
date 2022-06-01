4d43565435a21aff9ae64ca684ba031272c0329b
zavuto79 <106669738+zavuto79@users.noreply.github.com>
2022-06-01 19:23:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "gặp em trong mơ",
+    introTitle: 'em mây',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messDesc: '.',mỗi tối hàng đêm đều mơ về về bạn
+    btnAccept: 'đồ đáng ghét hihi <3',
+    messLink: 'https://www.facebook.com/messages/t/100026205905979/
 }
